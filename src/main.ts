f01887e1c9e98418400bb5b61932132a5b0d6da1
Ale Sánchez <alesanchezmedina@gmail.com>
2020-05-26 13:54:25 +0200

    Added global exception filter

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './interceptors/TimeoutInterceptor';
 import { Logger } from '@nestjs/common';
 import { NATSConfigService } from './config/NATSConfigService';
+import { AllExceptionsFilter } from './filters/ExceptionsFilter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { 
@@ -11,13 +13,16 @@ async function bootstrap() {
   });
 
   const natsConfigService: NATSConfigService = app.get(NATSConfigService);
+  const configService : ConfigService = app.get<ConfigService>(ConfigService);
 
   app.connectMicroservice({
     ...natsConfigService.getNATSConfig
   });
   app.use(helmet());
   app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   app.startAllMicroservicesAsync();
-  await app.listen(3000, () => Logger.log('Microservice running'));
+  await app.listen(configService.get<number>('PORT') || 3000, () => Logger.log('Microservice running'));
 }
 bootstrap();
